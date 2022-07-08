d57da5f1571e4c46bf6acfcac6d4f72c5a5e5ac4
Tuananhmaii <anhmttgcd201452@fpt.edu.vn>
2022-07-08 14:28:36 +0700

    adjust port 3

--- server.js
+++ server.js
@@ -11,11 +11,9 @@ const axios = require('axios')
 const { auth, requiresAuth } = require('express-openid-connect');
 const http = require('http')
 const host = 'localhost';
-const PORT = process.env.PORT || 3000;
 
-
-app.listen(PORT, host, function() {
-    console.log("Server started.......");
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
 // log requests 
