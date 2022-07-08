9b2b387ff9e149a7f54c8201b20adda1968454c7
Tuananhmaii <anhmttgcd201452@fpt.edu.vn>
2022-07-08 11:49:22 +0700

    adjust port 2

--- server.js
+++ server.js
@@ -64,9 +64,6 @@ app.get('/update',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{console.log(
-    'Server is running on http://localhost:3000')});
-
 app.use('/', require('./server/routes/router'))
 
 const config = {
